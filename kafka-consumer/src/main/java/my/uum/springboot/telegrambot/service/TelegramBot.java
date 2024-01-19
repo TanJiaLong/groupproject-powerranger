cd139b0dcc64826d125f325b3c13b922c6452c57
TanJiaLong <jialong999999@gmail.com>
2024-01-19 22:44:17 +0800

    Process active commenter and word count

--- kafka-consumer/src/main/java/my/uum/springboot/telegrambot/service/TelegramBot.java
+++ kafka-consumer/src/main/java/my/uum/springboot/telegrambot/service/TelegramBot.java
@@ -12,7 +12,11 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -20,6 +24,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     //No connect to database yet
     private final IssueService issueService;
+
     @Autowired
     public TelegramBot(BotConfig config, IssueService issueService) {
         this.config = config;
@@ -37,12 +42,12 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
-        if(update.hasMessage() && update.getMessage().hasText()){
+        if (update.hasMessage() && update.getMessage().hasText()) {
             Message userMessage = update.getMessage();
             long chatID = userMessage.getChatId();
 
 
-            switch(userMessage.getText()){
+            switch (userMessage.getText()) {
                 case "/start":
                     String welcomeMessage = "Hi ...";
                     sendMessage(chatID, welcomeMessage);
@@ -59,6 +64,16 @@ public class TelegramBot extends TelegramLongPollingBot {
                     //display all issue instance data from consumer by telegrambot
                     break;
                 case "/process":
+                    List<Issue> processIssues = issueService.getIssues();
+                    StringBuilder processMessage = new StringBuilder();
+
+                    String commenterCountMessage = displayActiveCommenter(processIssues);
+
+                    String wordCountMessage = displayWordCount(processIssues);
+
+                    processMessage.append(commenterCountMessage).append(wordCountMessage);
+
+                    sendMessage(chatID, processMessage.toString());
                     /**
                      * Display data in this format
                      *
@@ -89,15 +104,66 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-    private void displayIssues(long chatID, List<Issue> issues) {
-        StringBuilder issuesMessage = new StringBuilder("List of Issues:\n");
-        for(Issue issue: issues){
-            issuesMessage.append(String.format("%d. User: %s, Comment: %s\n", issue.getId(), issue.getUsername(), issue.getComment()));
-        }
+    private String displayActiveCommenter(List<Issue> processIssues) {
+        Map<String, Long> usernameOccurrences = countUsernames(processIssues);
+        // Print the results
+        List<Map.Entry<String, Long>> sortedEntries = usernameOccurrences.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .collect(Collectors.toList());
 
-        sendMessage(chatID, issuesMessage.toString());
+        StringBuilder activeCommenterMsg = new StringBuilder("List of active commenters\n");
+        for (int i = 0; i < sortedEntries.size(); i++) {
+            Map.Entry<String, Long> entry = sortedEntries.get(i);
+            activeCommenterMsg.append(i + 1).append(". ")
+                    .append(entry.getKey()).append(" [").append(entry.getValue()).append(" comments]\n");
+        }
+        activeCommenterMsg.append("\n");
+        return activeCommenterMsg.toString();
     }
 
+    private Map<String, Long> countUsernames(List<Issue> issues) {
+        return issues.stream()
+                .map(Issue::getUsername)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private String displayWordCount(List<Issue> processIssues) {
+        StringBuilder issuesString = new StringBuilder();
+
+        //Add all comment in 1 var for counting word
+        for (Issue issue : processIssues) {
+            issuesString.append(issue.getComment()).append(" ");
+        }
+
+        //List of Word Count
+        Map<String, Integer> wordOccurrence = countWord(issuesString.toString());
+        List<Map.Entry<String, Integer>> sortedEntries = wordOccurrence.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toList());
+
+        //Word Count Message
+        StringBuilder wordCount = new StringBuilder("List of word count\n");
+        for (int i = 0; i < sortedEntries.size(); i++) {
+            Map.Entry<String, Integer> entry = sortedEntries.get(i);
+            wordCount.append(i + 1).append(". ")
+                    .append(entry.getKey()).append(" [").append(entry.getValue()).append(" times]\n");
+        }
+
+        return wordCount.toString();
+    }
+
+    private static Map<String, Integer> countWord(String input) {
+        Map<String, Integer> occurrences = new HashMap<>();
+
+        // Remove special characters and split the input into words
+        String[] words = input.replaceAll("[^a-zA-Z\\s]", "").split("\\s+");
+
+        for (String word : words) {
+            occurrences.put(word, occurrences.getOrDefault(word, 0) + 1);
+        }
+
+        return occurrences;
+    }
     @Override
     public String getBotUsername() {
         return config.getUserName();
@@ -108,7 +174,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         return config.getToken();
     }
 
-    private void sendMessage(long chatID, String message){
+    private void sendMessage(long chatID, String message) {
         SendMessage botMessage = new SendMessage();
         botMessage.setChatId(chatID);
         botMessage.setText(message);
@@ -118,6 +184,15 @@ public class TelegramBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void displayIssues(long chatID, List<Issue> issues) {
+        StringBuilder issuesMessage = new StringBuilder("List of Issues:\n");
+        for (Issue issue : issues) {
+            issuesMessage.append(String.format("%d. User: %s, Comment: %s\n", issue.getId(), issue.getUsername(), issue.getComment()));
+        }
+
+        sendMessage(chatID, issuesMessage.toString());
     }
 }
 
